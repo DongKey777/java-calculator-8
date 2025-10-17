2d6b9f47236a5c5732e3fd9a66f5e7313f69d130
DongKey777 <higgs95@naver.com>
2025-10-17 15:38:02 +0900

    feat: 기본 구분자 통한 문자열 분리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,13 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         String inputStr = Calculator.input();
         System.out.println(inputStr);
+
+        String[] numbers = Calculator.parse(inputStr);
+        System.out.println(Arrays.toString(numbers));
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,8 +3,17 @@ package calculator;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Calculator {
+    private static final String DEFAULT_DELIMITERS = "[:,]";
 
     public static String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return readLine();
     }
+
+    public static String[] parse(String inputStr) {
+        String[] numbers = inputStr.split(DEFAULT_DELIMITERS);
+        return numbers;
+    }
+
+
 }
